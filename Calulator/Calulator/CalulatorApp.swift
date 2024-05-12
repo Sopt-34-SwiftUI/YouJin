e22b5eff81074b031c0b013e19cf3bd3e9b0f14d
youz2me <kynhun20@gachon.ac.kr>
2024-05-12 20:04:44 +0900

    Feat: 계산기 View 구현

---
+++ Calulator/Calulator/CalulatorApp.swift
@@ -0,0 +1,18 @@
+//
+//  CalulatorApp.swift
+//  Calulator
+//
+//  Created by YOUJIM on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CalulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CalulatorView()
+                .background(.black)
+        }
+    }
+}
